abbdd02711c38daf922356a49f7584abe9ecbfec
18jlli2 <18jlli2@stu.edu.cn>
2020-05-12 10:59:35 +0800

    sequelize test

--- sequelize_test.js
+++ sequelize_test.js
@@ -1,22 +1,75 @@
-var Sequelize = require('sequelize');
-
-var sequelize = new Sequelize('test', 'root', 'ljl094813.', {
-    host: 'localhost',
-    port:3306,
-    dialect: 'mysql'
+const Sequelize = require('sequelize');
+const config=require('./config');
+let sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
 });
-var Class1 = sequelize.define('class', {
-    Clno: Sequelize.INTEGER,
+//定义模型，告诉sequelize如何映射数据库表
+var Class = sequelize.define('class', {
+    Clno:{
+        type:Sequelize.INTEGER,
+        primaryKey:true
+    },
     Speciality: Sequelize.STRING,
-    Inyear: Sequelize.STRING
-});
-Class1.create({
-    Clno: 1,
-    Speciality: 'cs',
-    Inyear: '2018'
-}).then(function(result){
-    console.log('inserted XiaoMing ok');
-}).catch(function(err){
-    console.log('inserted XiaoMing error');
-    console.log(err.message);
-});
+    Inyear: Sequelize.STRING,
+    Number:Sequelize.STRING,
+    Monitor:Sequelize.STRING,
+}, {
+    timestamps: false});
+//插入数据
+
+( async ()=>{
+    var add=await Class.create({
+        Clno: '2',
+        Speciality: 'cs',
+        Inyear: '2018',
+        Number:'112',
+        Monitor:'2018612'
+    }).then(function(result){//插入成功
+        console.log('inserted success');
+    }).catch(function(err){//插入失败
+        console.log('inserted  error');
+        console.log(err.message);
+    });
+})();
+
+//查询数据
+
+(async () => {
+    var Classes = await Class.findAll({
+        where: {
+            //这里可以插入限制条件
+        }
+    });
+    console.log(`find ${Classes.length} classes:`);
+    for (let c of Classes) {
+        console.log(JSON.stringify(c));
+    }
+})();
+
+
+//更新数据
+(async () => {
+    var Classes = await Class.update({
+        Number:'123'   //修改的内容
+    }, {//下面写限制条件
+        where:{
+            Clno:'1'
+        }
+    })
+})();
+
+//删除数据
+(async () => {
+    var Classes = await Class.destroy({
+        //下面写限制条件
+        where:{
+            Clno:'2'
+        }
+    })
+})();
